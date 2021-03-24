902db0ba411c09843356bb926e010975ed627f30
Yeon4905 <lentsal9780@gmail.com>
2021-03-24 15:50:09 +0900

    couldn't fix the result of empty lists(dataframes) - excel sumproduct

--- weather_load_container_2020.py
+++ weather_load_container_2020.py
@@ -8,11 +8,11 @@ tqdm = partial(tqdm, position = 0, leave = True )
 
 df_dir = 'C:\\Users\\HYU\\Desktop\\Lab\\OneDrive - 한양대학교\\해수부\\사고예측\\2020년 BPT\\'
 os.chdir(df_dir)
-ship_gam = pd.read_csv("선석배정_감만_utf_nan_drop.csv", sep=',', 
-                       usecols = ['입항날짜', '입항시간','출항날짜','출항시간','선적','양하'], 
+ship_gam = pd.read_csv("선석배정_감만_utf_nan_drop.csv", sep=',',
+                       usecols = ['입항날짜', '입항시간','출항날짜','출항시간','선적','양하'],
                        encoding = 'ANSI')
-ship_shin = pd.read_csv("선석배정_신선대_utf_nan_drop.csv", sep=',', 
-                       usecols = ['입항날짜', '입항시간','출항날짜','출항시간','선적','양하'], 
+ship_shin = pd.read_csv("선석배정_신선대_utf_nan_drop.csv", sep=',',
+                       usecols = ['입항날짜', '입항시간','출항날짜','출항시간','선적','양하'],
                        encoding = 'ANSI')
 
 
@@ -112,36 +112,36 @@ def search_time(k, In, haul):
 if __name__ == "__main__":
     in_times_shin = defaultdict(list)
     in_days_shin = defaultdict(list)
-    Days_shin = ['D' + str(i) for i in range(1, 1233)] 
-  
+    Days_shin = ['D' + str(i) for i in range(1, 1233)]
+
     in_times_gam = defaultdict(list)
     in_days_gam = defaultdict(list)
-    Days_gam = ['D' + str(i) for i in range(1, 1233)] 
+    Days_gam = ['D' + str(i) for i in range(1, 1233)]
 
     # Days = defaultdict(DataFrame)
     # change_date(ship_gam)
     Days = [[] for _ in range(1, 1233)]
     # times = [[0 for _ in range(0, 2400, 100)] for _ in range(len(Days[k]))]
 
-    
+
     for i in tqdm(range(len(ship_gam))):
         in_gam = ship_gam.iloc[i]
-        in_date_ = ship_gam.iloc[i]['입항날짜']    
+        in_date_ = ship_gam.iloc[i]['입항날짜']
         in_day = ship_gam.iloc[i]['입항날짜'] -20200000
         in_days_gam[in_day].append(ship_gam.iloc[i])
         # times[i] = search_date_time(in_day, in_days, times[i])
         # In_days = pd.DataFrame([[k] + j for k,v in in_days.items() for j in v], columns = ['입항날짜', '입항시간','양하'])
         # Days_gam[in_day] = pd.DataFrame(in_days_gam[in_day])
         # dict_to_dataframe(in_days_gam, Days_gam)
-        
+
     for j in tqdm(range(len(ship_shin))):
         in_shin = ship_shin.iloc[j]
-        in_date = ship_shin.iloc[j]['입항날짜']    
+        in_date = ship_shin.iloc[j]['입항날짜']
         in_day = ship_shin.iloc[j]['입항날짜'] -20200000
-        in_days_gam[in_day].append(ship_shin.iloc[j])       
+        in_days_gam[in_day].append(ship_shin.iloc[j])
         # Days_shin[in_day] = pd.DataFrame(in_days_shin[in_day])
     dict_to_dataframe(in_days_gam, Days)
-    
+
     for k in tqdm(range(len(in_days_gam))):
         # times = [[0 for _ in range( for _ in range(1, 1233)]
         # Times = [0 for _ in range(1, 1233)]
@@ -201,20 +201,20 @@ if __name__ == "__main__":
                 times[k][22] += haul
             elif In < 2400:
                 times[k][23] += haul
-            
-        # print("time-" + str(k) +":", times[k])
+
+            print("time-" + str(k) +":", times[k])
             # times[k] = (search_time(k, In, haul))
             # print("time-" + str(k) +":", times[k])
             # Times[k] = pd.DataFrame(times[k])
             # Times[k].to_csv(df_dir + "양하\\양하" + str(k) + ".csv")
-            # Days['sum_haul'] = times[i] 
+            # Days['sum_haul'] = times[i]
     # for k in tqdm(range(1, 1233)):
     #     if not Days_gam[k] or Days_shin[k]:
     #         continue
     #     else:
     #         pd.merge([Days_gam, Days_shin])
     #     # Days_gam[in_day].to_csv(df_dir + "감만_new\\선석배정_감만" + str(in_day) + ".csv", columns = ['입항날짜', '입항시간', '양하'], index = False, encoding = 'ANSI')
-    
+
 # print(in_days[101])
     # for j in range(len(in_days)):
     #     Days[j] = dict_to_dataframe(in_days, Days)
